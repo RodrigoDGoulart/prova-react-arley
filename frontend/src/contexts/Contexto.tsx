120fb96953dfc9321138af5941975f048ceb0c31
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-06-12 10:52:34 -0300

    style: warning comment

--- frontend/src/contexts/Contexto.tsx
+++ frontend/src/contexts/Contexto.tsx
@@ -6,6 +6,7 @@ const Contexto = createContext({} as ContextProps);
 function Provider({ children }: any) {
   const [letters, setLetters] = useState([] as LetterProps[]);
 
+  // fazer requisição get aqui!!! não fazer nos outros componentes, use useEffect!!!!
   return (
     <Contexto.Provider value={{ letters, setLetters }}>
       {children}
